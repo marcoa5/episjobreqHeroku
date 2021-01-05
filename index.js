0fce20eefc355ea18fd94e9de93cf50541baf60e
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-01-05 19:01:03 +0100

    2

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var app = express();
-const port= 2000;
 
 app.get('/', function(req, res,next) {
     res.status(200).json({stato: 'ok'});
 });
 
-app.get('*', function(req, res,next) {
-    res.status(404).json({stato: 'NOK'})
-  });
-
-app.listen(port, ()=>{
+app.listen(2000, ()=>{
     console.log('RUNNING')
 });
