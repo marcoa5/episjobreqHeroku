e913c089d718c57f5d3c73385ba70900f45f0acf
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-01-06 22:59:08 +0100

    13

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const transporter = nodemailer.createTransport({
   });
 
 
-function createMailOptions(to1){
+function createMailOptions(to1, subject, son1,son2,son3){
     const mailOptions = {
         from: 'Epiroc Service <episerjob@gmail.com>',
         to: to1,
-        subject: 'Invoices due',
-        text: 'Dudes, we really need your money.'
+        subject: subject,
+        text: "In allegato scheda lavoro relativa all'intervento da noi effettuato.\nVi ringraziamo qualora abbiate aderito al nostro sondaggio."  + "\n\n\nRisultato sondaggio:\n\nOrganizzazione intervento: " + son1 + "\nConsegna Ricambi: " + son2 + "\nEsecuzione Intervento: " + son3
       };
       return mailOptions
 }
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 app.get('/user', function(req, res,next) {
     if(req.query.to1!=undefined){
-        transporter.sendMail(createMailOptions(req.query.to1), (error, info)=>{
+        transporter.sendMail(createMailOptions(req.query.to1, req.query.subject, req.query.son1, req.query.son2,req.query.son3), (error, info)=>{
             if (error) {
             console.log(error);
             } else {
