6c992bb5f5350313fd4524b00843fa5af006a472
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-01-06 11:07:38 +0100

    8

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res,next) {
+app.get('/user', function(req, res,next) {
+    if(req!=undefined){res.send(req.body)}
     res.status(200).json({stato: 'ok'});
     res.end();
 });
 
 app.get('*', function(req, res,next) {
-    if(req!=undefined){res.send(req.body)}
     res.status(404).json({stato: 'Nok'});
     res.end();
 });
