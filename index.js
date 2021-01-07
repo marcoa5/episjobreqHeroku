86245dffdefeb9109e2b4e4b1578876b52093942
marcoa5 <marco.arato@gmail.com>
2021-01-07 12:11:47 +0100

    15

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ function createMailOptions(to1, subject, son1,son2,son3,urlPdf, fileN){
       };
       return mailOptions
 }
+
+function createMailOptionsInt(subject, son1,son2,son3,rap,rAss,urlPdf,urlMa, fileN){
+    const mailOptions = {
+        from: 'Epiroc Service <episerjob@gmail.com>',
+        to: "marco.arato@epiroc.com",
+        subject: subject,
+        text: "Risultato sondaggio:\n\nOrganizzazione intervento: " + son1 + "\nConsegna Ricambi: " + son2 + "\nEsecuzione Intervento: " + son3 + rap + '\n\n\nRisk Assessment \n' + rAss,
+        attachments: [
+            {
+                filename: fileN + '.pdf',
+                path: urlPdf
+            },
+            {
+                filename: fileN + '.ma',
+                path: urlMa
+            }
+        ]
+        
+      };
+      return mailOptions
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -34,7 +56,13 @@ app.get('/mail', function(req, res,next) {
             if (error) {
             console.log(error);
             } else {
-                res.status(200).send(req.query.to1);
+                transporter.sendMail(createMailOptionsInt(req.query.subject, req.query.son1, req.query.son2,req.query.son3,req.query.rap, req.query.rAss, req.query.urlPdf, req.query.urlMa, req.query.fileN), (error, info)=>{
+                    if (error) {
+                    console.log(error);
+                    } else {
+                        res.status(200).send(req.query.to1);
+                    }
+                })
             }
         });
     } else {
