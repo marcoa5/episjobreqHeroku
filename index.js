36d19ac8aabb7818c82c39cd6b75a06a46febea2
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-01-15 16:23:02 +0100

    f

--- index.js
+++ index.js
@@ -49,6 +49,27 @@ function createMailOptionsInt(subject, son1,son2,son3,rap,rAss,urlPdf,urlMa, fil
       return mailOptions
 }
 
+function createMailOptionsIntProd(subject, son1,son2,son3,rap,rAss,urlPdf,urlMa, fileN){
+    const mailOptions = {
+        from: 'Epiroc Service <episerjob@gmail.com>',
+        to: "marco.fumagalli@epiroc.com",
+        cc: "mario.parravicini@epiroc.com; carlo.colombo@epiroc.com; marco.arato@epiroc.com",
+        subject: subject,
+        text: "Risultato sondaggio:\n\nOrganizzazione intervento: " + son1 + "\nConsegna Ricambi: " + son2 + "\nEsecuzione Intervento: " + son3 + rap + '\n\n\nRisk Assessment \n' + rAss,
+        attachments: [
+            {
+                filename: fileN + '.pdf',
+                path: urlPdf
+            },
+            {
+                filename: fileN + '.ma',
+                path: urlMa
+            }
+        ]
+      };
+      return mailOptions
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -70,6 +91,30 @@ app.get('/mail', function(req, res,next) {
     } else {
         res.send('Mail not sent')
     }
+});
+
+app.get('/mailprod', function(req, res,next) {
+    if(req.query.to1!=undefined){
+        transporter.sendMail(createMailOptions(req.query.to1, req.query.subject, req.query.son1, req.query.son2,req.query.son3, req.query.urlPdf, req.query.fileN, req.query.userN, req.query.userC, req.query.userM), (error, info)=>{
+            if (error) {
+            console.log(error);
+            } else {
+                transporter.sendMail(createMailOptionsIntProd(req.query.subject, req.query.son1, req.query.son2,req.query.son3,req.query.rap, req.query.rAss, req.query.urlPdf, req.query.urlMa, req.query.fileN), (error, info)=>{
+                    if (error) {
+                    console.log(error);
+                    } else {
+                        res.status(200).send(req.query.to1);
+                    }
+                })
+            }
+        });
+    } else {
+        res.send('Mail not sent')
+    }
+});
+
+app.get('/users', function(req, res,next) {
+    res.send('Ok')
 });
 
 app.get('*', function(req, res,next) {
