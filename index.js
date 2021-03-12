722e3ac51aca9ff342ebc75bec82cfbeccbd370e
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-03-12 16:53:15 +0100

    o

--- index.js
+++ index.js
@@ -114,20 +114,20 @@ app.all('/rendersj', (req,res)=>{
     res.status(200).send(o(i))
 })
 
-app.all('/rendersjpdf', (req,res)=>{
+app.all('/sjpdf', (req,res)=>{
     var t = fs.readFileSync('template.html','utf-8')
     var i = req.body
     var o = Handlebars.compile(t)
     const file = {content: o(i)}
     const options = {format: 'A4'}
     htp.generatePdf(file,options, buf=>{
-        var path = __dirname + '/temp/temp.pdf'
+        var path = __dirname + '/temp.pdf'
         fs.writeFileSync(path, buf)
         res.status(200).sendFile(path)
     })
 })
 
-app.all('/rendersjpdf1', (req,res)=>{
+app.all('/sjpdf1', (req,res)=>{
     var t = fs.readFileSync('template.html','utf-8')
     var i = req.body
     var o = Handlebars.compile(t)
