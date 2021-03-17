cfd5ff9be4f2fb605e8ac60f753533dfe4a321cf
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-03-17 15:48:45 +0100

    2

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.all('/sjpdf', (req,res)=>{
     const options = {format: 'A4'}
     hpt.generatePdf(file,options)
     .then(a=>{
-        res.send(a.toString())
+        res.send(a)
     })
 })
 
