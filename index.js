a9b938ae043a9e3054c1745fe3e60f6922f405f3
marcoa5 <marco.arato@gmail.com>
2021-07-13 21:17:47 +0200

    k

--- index.js
+++ index.js
@@ -104,8 +104,26 @@ function createMailOptionsIntProd(a){
       return mailOptions
 }
 
-app.use(bodyParser.urlencoded({limit: '5MB',extended: false}))
+app.use(bodyParser.urlencoded({limit: '100kb',extended: false}))
 app.use(bodyParser.json())
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.all('/rendersj', (req,res)=>{
     var t = fs.readFileSync('template.html','utf-8')
